7d2df230d0a7cd2bd47a6a314371863b51b32bcb
SamuelPerez27 <samuelpperez07@gmail.com>
2021-03-28 03:56:36 -0300

    Nave Movimiento

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float speed;
+    Vector2 input;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        input.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = input * speed * Time.fixedDeltaTime;
+    
+     }
+}
