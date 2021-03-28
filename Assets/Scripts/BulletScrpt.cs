ac5168edbd5fd8533ceaf39e1190451f5a1da7d3
LuisPilier <70179807+LuisPilier@users.noreply.github.com>
2021-03-28 17:37:51 -0400

    Disparo de la nave

---
+++ Assets/Scripts/BulletScrpt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScrpt : MonoBehaviour
+{
+    private Rigidbody2D Rigidbody2D;
+    public float Speed;
+    private Vector2 Direction;
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = Vector2.left * Speed;
+    }
+    public void SetDirection(Vector2 direction)
+	{
+        Direction = direction;
+	}
+    public void DestroyBullet()
+	{
+        Destroy(gameObject);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public GameObject BulletPrefab;
     private Rigidbody2D rb;
     public float speed;
     Vector2 input;
+    private float LastShoot;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +21,25 @@ public class PlayerController : MonoBehaviour
     {
 
         input.y = Input.GetAxis("Vertical");
+
+
+		if (Input.GetKey(KeyCode.Space) && Time.time > LastShoot + 0.25f)
+		{
+            Shoot();
+            LastShoot = Time.time;
+		}
     }
 
     private void FixedUpdate()
     {
         rb.velocity = input * speed * Time.fixedDeltaTime;
     
-     }
+    }
+    private void Shoot()
+	{
+        
+        
+      Instantiate(BulletPrefab, transform.position * 0.7f, Quaternion.identity);
+        
+	}
 }
