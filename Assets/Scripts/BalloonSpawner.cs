f654c0469b9bcd9b25f17677801c2d7824d3916b
Joao Alberto Navarro Sanchez <jans101001@hotmail.com>
2021-03-27 21:08:14 -0400

    BalloonSpawn

---
+++ Assets/Scripts/BalloonSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonSpawner : MonoBehaviour
+
+{
+
+    [SerializeField] GameObject balloonPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0.0f, .6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn() {
+        Instantiate(balloonPrefab, transform.position, transform.rotation);
+    
+    }
+}
