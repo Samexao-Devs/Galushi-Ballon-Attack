641d9940271e0ae593400b67c9a3b0e08e1099c4
SamuelPerez27 <samuelpperez07@gmail.com>
2021-03-29 03:09:14 -0300

    Menu

---
+++ Assets/Scripts/Controladorescena.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controladorescena : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void CambiarEscena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+}
