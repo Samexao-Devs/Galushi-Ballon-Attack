23f5a6dc6d2a72fc47f6f37fcd6aa9ba53bbec37
LuisPilier <70179807+LuisPilier@users.noreply.github.com>
2021-03-30 02:01:25 -0400

    Update GameOverScreen.cs

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -9,9 +9,13 @@ public class GameOverScreen : MonoBehaviour
 	public void RestartButton()
 	{
 		SceneManager.LoadScene("SampleScene");
+		PlayerController.vidaActual = 10;
+		ScoreScript.scoreValue = 0;
 	}
 	public void MainButton()
 	{
 		SceneManager.LoadScene("New Scene");
+		PlayerController.vidaActual = 10;
+		ScoreScript.scoreValue = 0;
 	}
 }
